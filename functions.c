0de077883f5613240545e38c8f5bb6fa9786dc2d
unknown <dhruvinsojitra11@gmail.com>
2023-09-23 16:31:30 +0530

    second commit

---
+++ functions.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void smallest()
+{
+    int num1,num2,num3,a;
+
+    printf("enter num 1:");
+    scanf("%d",&num1);
+    printf("enter num 2:");
+    scanf("%d",&num2);
+    printf("enter num 3:");
+    scanf("%d",&num3);
+
+    if(a<12)
+    {
+        printf("yes its small");
+    }
+    else
+    {
+
+        printf("its big");
+    }
+
+
+}
+
+
+int main()
+
+{
+    smallest();
+    return 0;
+}
